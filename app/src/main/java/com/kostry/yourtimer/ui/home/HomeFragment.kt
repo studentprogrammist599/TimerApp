37aa591ef875dbce196abf2cf57084627fdb6ed0
kosykt <70757252+kosykt@users.noreply.github.com>
2022-09-03 00:11:00 +0900

    add worker

--- app/src/main/java/com/kostry/yourtimer/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kostry/yourtimer/ui/home/HomeFragment.kt
@@ -1,6 +1,11 @@
 package com.kostry.yourtimer.ui.home
 
+import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
+import com.kostry.yourtimer.TimerWorker
 import com.kostry.yourtimer.databinding.FragmentHomeBinding
 import com.kostry.yourtimer.ui.base.BaseFragment
 
@@ -8,5 +13,15 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun getViewBinding(container: ViewGroup?): FragmentHomeBinding {
         return FragmentHomeBinding.inflate(layoutInflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val workManager = WorkManager.getInstance(requireContext())
+        workManager.enqueueUniqueWork(
+            TimerWorker.TIMER_WORKER_NAME,
+            ExistingWorkPolicy.REPLACE,
+            TimerWorker.makeRequest(0, 10)
+        )
     }
 }
